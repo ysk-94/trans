fb4f1013c1ce56529359ed08db33d7f672d67c21
sakamura yosuke <yosuke_sakamura@waku-2.com>
2020-03-04 17:44:43 +0900

    できた

--- trans.cpp
+++ trans.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include "httplib.h"
+#include <string>
 
-int main() {
+int main(int args, char* argv[]) {
+	std::cout << argv[1] << std::endl;
+
+	if (args < 1) return 1;
+
 	httplib::Client cli("api.codic.jp");
 	httplib::Headers headers = {
 		{ "Authorization", "Bearer sxvj7zYmzo6OEYfBCgzOGfKoz10XM0bJgz" }
 	};
 
-	auto res = cli.Get("/v1/engine/translate.json?text=こんにちは", headers);
+	char url[] = "/v1/engine/translate.json?text=";
+
+	auto res = cli.Get(strcat(url, argv[1]), headers);
 	std::cout << res->status << std::endl;
 	if (res && res->status == 200) {
 		std::cout << res->body << std::endl;
