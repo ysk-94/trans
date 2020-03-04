12640bb013957d89cc99d74c4563bcd9fd820097
sakamura yosuke <yosuke_sakamura@waku-2.com>
2020-03-04 17:34:21 +0900

    データ取れた

--- trans.cpp
+++ trans.cpp
@@ -2,9 +2,13 @@
 #include "httplib.h"
 
 int main() {
-	httplib::Client cli("www.google.com");
+	httplib::Client cli("api.codic.jp");
+	httplib::Headers headers = {
+		{ "Authorization", "Bearer sxvj7zYmzo6OEYfBCgzOGfKoz10XM0bJgz" }
+	};
 
-	auto res = cli.Get("/");
+	auto res = cli.Get("/v1/engine/translate.json?text=こんにちは", headers);
+	std::cout << res->status << std::endl;
 	if (res && res->status == 200) {
 		std::cout << res->body << std::endl;
 	}
